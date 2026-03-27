ca1c834d32b7ae22eac50a0c26b51aa3a6441e51
panda850819 <pandap.d819@gmail.com>
2026-03-28 00:16:15 +0800

    fix: initialize env_logger for debug output

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1253,6 +1253,8 @@ fn open_settings(app: tauri::AppHandle) {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    env_logger::init();
+
     #[allow(unused_mut)]
     let mut builder = tauri::Builder::default()
         .plugin(tauri_plugin_opener::init());
